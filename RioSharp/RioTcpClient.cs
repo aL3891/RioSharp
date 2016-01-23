f2450507cde3cd0eb212f5caa08dcd29ebce4232
Allan Lindqvist <al_3891@hotmail.com>
2016-01-23 02:07:58 +0100

    Added classes for tcp and udp client (empty so far)

---
+++ RioSharp/RioTcpClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RioSharp
+{
+    public class RioTcpClient : RioTcpSocketPool
+    {
+        public RioTcpClient(RioFixedBufferPool sendPool, RioFixedBufferPool revicePool, int socketCount,
+            uint maxOutstandingReceive = 1024, uint maxOutstandingSend = 1024, uint maxConnections = 1024)
+            : base(sendPool, revicePool, socketCount, maxOutstandingReceive, maxOutstandingSend, maxConnections)
+        {
+
+        }
+    }
+}
